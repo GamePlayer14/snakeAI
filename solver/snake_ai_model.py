94c75a19d7a0f3c469ad8b7652d3b2d5094481c0
Tyler <tylermetzger21@gmail.com>
2025-04-01 17:41:51 -0700

    fixed inputs

--- solver/snake_ai_model.py
+++ solver/snake_ai_model.py
@@ -14,9 +14,10 @@ def build_model(input_size, output_size):
     ])
     return model
 
-def get_features(game_state):
-    # Step 1: Flatten the board state to 1601 elements
-    board_vector = np.zeros((1601,), dtype=np.float32)  # TODO: Replace with actual encoding
+def get_features(game_state,  game_shape):
+    # Step 1: Flatten the board state to 1600 elements
+    board_vector = np.zeros(game_shape, dtype=np.float32)
+    board_vector = get_state(game_state, board_vector)
 
     # Step 2: Append apple distance
     head_y, head_x = game_state.snake[0]
@@ -40,3 +41,14 @@ def get_action(model, game_state):
     prediction = model.predict(features.reshape(1, -1), verbose=0)
     relative_action = np.argmax(prediction)
     return relative_to_absolute(game_state.direction, relative_action)
+
+def get_state(game_state, _0list):
+    i = 1.0
+    for y, x in game_state.snake:
+        _0list[y][x] = i
+        i -= 0.001
+    return _0list
+        
+
+
+
